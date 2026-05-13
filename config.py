5199b84ef0d4046146bc1aebf2fcda361652c07d
torgash1221 <torgash1221@gmail.com>
2026-05-13 23:52:53 +0300

    Add configuration settings for Binance and Telegram

---
+++ config.py
@@ -0,0 +1,34 @@
+# =========================
+# BINANCE SETTINGS
+# =========================
+
+SYMBOLS = [
+    "BTC/USDT",
+    "ETH/USDT",
+    "SOL/USDT",
+    "WLD/USDT"
+]
+
+TIMEFRAME = "15m"
+
+WINDOW_SIZE = 50
+
+
+# =========================
+# PATTERN DETECTION
+# =========================
+
+ALERT_THRESHOLD = 87
+
+WATCH_THRESHOLD = 75
+
+PATTERN_FOLDER = "patterns"
+
+
+# =========================
+# TELEGRAM
+# =========================
+
+TELEGRAM_BOT_TOKEN = ""
+
+TELEGRAM_CHAT_ID = ""
