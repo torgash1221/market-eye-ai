e95c6bd381a30af02dc04b38a717987bf63fb534
torgash1221 <torgash1221@gmail.com>
2026-05-15 01:21:41 +0300

    Update binance_feed.py

--- detector/exchange/binance_feed.py
+++ detector/exchange/binance_feed.py
@@ -9,12 +9,20 @@ exchange = ccxt.binance({
 })
 
 
-def get_closes(symbol, timeframe, limit):
+def get_ohlcv(symbol, timeframe, limit):
+
     candles = exchange.fetch_ohlcv(
         symbol=symbol,
         timeframe=timeframe,
         limit=limit
     )
+
+    return candles
+
+
+def get_closes(symbol, timeframe, limit):
+
+    candles = get_ohlcv(symbol, timeframe, limit)
 
     closes = [candle[4] for candle in candles]
 
