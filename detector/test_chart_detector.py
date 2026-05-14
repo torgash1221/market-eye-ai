3ada4929e5d66b7a2feb21cbc7fe5aee49fc0faf
torgash1221 <torgash1221@gmail.com>
2026-05-14 03:02:21 +0300

    Update test_chart_detector.py

--- detector/test_chart_detector.py
+++ detector/test_chart_detector.py
@@ -1,11 +1,14 @@
 from detector.chart_renderer import render_chart
 from detector.image_detector import detect_image_pattern
+from exchange.binance_feed import get_closes
 
 
-closes = [
-    100, 98, 96, 94, 95, 97, 99,
-    97, 95, 94, 96, 99, 103, 106
-]
+SYMBOL = "BTCUSDT"
+TIMEFRAME = "1m"
+WINDOW = 100
+
+
+closes = get_closes(SYMBOL, TIMEFRAME, WINDOW)
 
 image_path = render_chart(closes)
 
