25c8748136cc9209b4435644ea6b89eec2169975
torgash1221 <torgash1221@gmail.com>
2026-05-15 16:12:28 +0300

    Update chart_renderer.py

--- detector/chart_renderer.py
+++ detector/chart_renderer.py
@@ -1,25 +1,28 @@
 import os
 import pandas as pd
 import mplfinance as mpf
-from datetime import datetime
+from datetime import datetime, UTC
 
 
 def render_chart(candles, filename="current_chart.png"):
+
     os.makedirs("charts", exist_ok=True)
 
     rows = []
 
     for candle in candles:
+
         rows.append({
-            "Date": datetime.utcfromtimestamp(candle[0] / 1000),
-            "Open": candle[1],
-            "High": candle[2],
-            "Low": candle[3],
-            "Close": candle[4],
-            "Volume": candle[5],
+            "Date": datetime.fromtimestamp(candle[0] / 1000, UTC),
+            "Open": float(candle[1]),
+            "High": float(candle[2]),
+            "Low": float(candle[3]),
+            "Close": float(candle[4]),
+            "Volume": float(candle[5]),
         })
 
     df = pd.DataFrame(rows)
+
     df.set_index("Date", inplace=True)
 
     image_path = os.path.join("charts", filename)
@@ -29,8 +32,15 @@ def render_chart(candles, filename="current_chart.png"):
         type="candle",
         style="charles",
         volume=False,
+        figsize=(8, 5),
+        tight_layout=True,
         axisoff=True,
-        savefig=dict(fname=image_path, dpi=120, bbox_inches="tight")
+        savefig=dict(
+            fname=image_path,
+            dpi=150,
+            bbox_inches="tight",
+            pad_inches=0
+        )
     )
 
     return image_path
